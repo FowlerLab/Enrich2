0b84eefc0bfb04af0c44b8472ae04bc7d9060255
Alan Rubin <afrubin@uw.edu>
2016-12-18 22:16:13 +1100

    Linting

--- enrich2/gui/create_seqlib_dialog.py
+++ enrich2/gui/create_seqlib_dialog.py
@@ -36,9 +36,8 @@ from ..variant import VariantSeqLib
 seqlib_label_text = OrderedDict([("BcvSeqLib", "Barcoded Variant"),
                                  ("BcidSeqLib", "Barcoded Identifier"),
                                  ("OverlapSeqLib", "Overlap"),
-                                 ("BasicSeqLib", "Basic"), 
+                                 ("BasicSeqLib", "Basic"),
                                  ("BarcodeSeqLib", "Barcodes Only")])
-
 
 
 class CreateSeqLibDialog(tkSimpleDialog.Dialog):
@@ -50,17 +49,16 @@ class CreateSeqLibDialog(tkSimpleDialog.Dialog):
         self.element_type = None
         tkSimpleDialog.Dialog.__init__(self, parent_window, title)
 
-
     def body(self, master):
         message = ttk.Label(master, text="SeqLib type:")
         message.grid(column=0, row=0)
 
         for i, k in enumerate(seqlib_label_text.keys()):
-            rb = ttk.Radiobutton(master, text=seqlib_label_text[k], variable=self.element_tkstring, value=k)
+            rb = ttk.Radiobutton(master, text=seqlib_label_text[k],
+                                 variable=self.element_tkstring, value=k)
             rb.grid(column=0, row=(i + 1), sticky="w")
             if i == 0:
                 rb.invoke()
-
 
     def buttonbox(self):
         """
@@ -68,13 +66,13 @@ class CreateSeqLibDialog(tkSimpleDialog.Dialog):
         """
         box = tk.Frame(self)
 
-        w = tk.Button(box, text="OK", width=10, command=self.ok, default="active")
+        w = tk.Button(box, text="OK", width=10, command=self.ok, 
+                      default="active")
         w.pack(side="left", padx=5, pady=5)
 
         self.bind("<Return>", self.ok)
 
         box.pack()
-
 
     def apply(self):
         try:
